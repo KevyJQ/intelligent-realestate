9e68354f533ec86e3d794300bee2a58ab96c9ded
Kevy <kevy.bryan@gmail.com>
2022-10-23 15:06:15 -0500

    Implementacion del controlador de crearArrendador
    Creamos la pagina html para crear Arrendador
    Agregamos el boton en la pagina principal para Crear Arrendador

--- src/main/java/com/intelligent/realestate/controllers/ArrendadorController.java
+++ src/main/java/com/intelligent/realestate/controllers/ArrendadorController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,6 +53,19 @@ public class ArrendadorController {
 	@RequestMapping(value = "/guardarCambioArrendador", method = RequestMethod.POST)
 	public String guardarArrendador(@ModelAttribute("arrendador") Arrendador arrendador) {
 		arrendadorDao.actualizarArrendador(arrendador);
+		return "redirect:/arrendadores";
+	}
+	
+	@RequestMapping("/crearArrendador")
+	public String CrearArrendador(Model model) {
+		Arrendador arrendador = new Arrendador();
+		model.addAttribute("arrendador", arrendador);
+		return "crearArrendador";
+	}
+	
+	@RequestMapping(value = "/guardarNuevoArrendador", method = RequestMethod.POST)
+	public String guardarNuevoArrendador(@ModelAttribute("arrendador") Arrendador arrendador) {
+		arrendadorDao.guardarArrendador(arrendador);
 		return "redirect:/arrendadores";
 	}
 }
